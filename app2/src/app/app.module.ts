a73ae2113055122c001376436890bc02c9bf0952
Oterem <ot1234ster@gmail.com>
2017-03-09 15:45:33 +0200

    targil2

---
+++ app2/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { AppComponent } from './app.component';
+import { NavItemComponent } from './nav-item/nav-item.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavItemComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
